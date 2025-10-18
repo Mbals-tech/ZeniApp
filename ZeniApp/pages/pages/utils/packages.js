b9e5cbdc8d9120df3093dcde8380c59ad8b0baef
Mbals-tech <138695374+Mbals-tech@users.noreply.github.com>
2025-10-18 16:55:25 +0200

    Update packages.js

--- ZeniApp/pages/pages/utils/packages.js
+++ ZeniApp/pages/pages/utils/packages.js
@@ -1,1 +1,7 @@
-
+export const PACKAGES = {
+  starter: { key:'starter', deposit:20, dailyReturn:60, days:45 },
+  starter_pro: { key:'starter_pro', deposit:80, dailyReturn:110, days:45 },
+  standard: { key:'standard', deposit:1800, dailyReturn:300, days:45 },
+  premium: { key:'premium', deposit:2000, dailyReturn:600, days:45 },
+  elite: { key:'elite', deposit:5000, dailyReturn:1500, days:45 },
+};
