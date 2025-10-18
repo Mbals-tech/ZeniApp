bd34b93a812448306d5cd8d3cea1683a5b168e40
Mbals-tech <138695374+Mbals-tech@users.noreply.github.com>
2025-10-18 21:18:11 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{
+      display: "flex",
+      height: "100vh",
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column",
+      fontFamily: "sans-serif"
+    }}>
+      <h1>Welcome to ZeniApp 🚀</h1>
+      <p>This is your main landing page.</p>
+    </div>
+  );
+}
