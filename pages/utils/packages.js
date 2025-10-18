b9d8c88af581e871afa5356d2e4eb66a382bea01
Mbals-tech <138695374+Mbals-tech@users.noreply.github.com>
2025-10-18 22:44:13 +0200

    Update packages.js

--- pages/utils/packages.js
+++ pages/utils/packages.js
@@ -1,4 +1,4 @@
-export const PACKAGES = {
+export const packages = {
   starter:    { key:'starter', deposit:200, dailyReturn:60, days:45 },
   starter_pro:{ key:'starter_pro', deposit:500, dailyReturn:150, days:45 },
   standard:   { key:'standard', deposit:1000, dailyReturn:300, days:45 },
